dd1b7c7203ddb320aaa4c209239fd41ba938442e
YoseopLee <80019249+YoseopLee@users.noreply.github.com>
2021-12-25 09:28:03 +0900

    [add]pop-new

--- src/Pages/Post/Post.jsx
+++ src/Pages/Post/Post.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import MainBar from "../../Components/Common/Mainbar/MainBar";
 import Navbar from "../../Components/Common/Navbar/Navbar";
@@ -7,14 +7,20 @@ import PostPop from "./PostPop";
 import PostNew from "./PostNew";
 
 const Post = () => {
-    
+    const [showList, setShowList] = useState(false);
     return (
         <>
             <MainBar />
             <Navbar />
-            <div className="pop-new">
-                <div className="pop-tab"><Link to="">인기</Link></div>
-                <div className="new-tab"><Link to="new">최신</Link></div>
+            <div className="sort-list">
+                <img src="images/updown.png" className="updown-btn" alt="" onClick={() => setShowList(!showList)}/>
+                {
+                    showList && 
+                    <ul>
+                        <Link to=""><li className="pop-tab">인기</li></Link>
+                        <Link to="new"><li className="new-tab">최신</li></Link>
+                    </ul>
+                }
             </div>
             <Routes>
                 <Route path="" element={<PostPop/>} />
