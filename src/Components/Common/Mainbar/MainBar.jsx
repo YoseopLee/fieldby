6882c79bc66c56cd135dc174a156ee2d3c165656
YoseopLee <80019249+YoseopLee@users.noreply.github.com>
2022-01-04 17:26:15 +0900

    [add]kakao-login 완성

--- src/Components/Common/Mainbar/MainBar.jsx
+++ src/Components/Common/Mainbar/MainBar.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { SidebarData } from './MainbarData';
+import { IntroSidebarData, LoginSidebarData } from './MainbarData';
 import './Mainbar.css';
+import { useDispatch } from 'react-redux';
+import { actionCreators as userActions } from "../../../Redux/Reducers/user";
+import { history } from '../../../Redux/configureStore';
 
 
 const MainBar = () => {
+    const dispatch = useDispatch();
+    
+    // Login 상태
+    const status = localStorage.getItem("token");
+    const is_login = status === null ? false : true;
+
     const [sidebar, setSidebar] = useState(false);
     const [black, setBlack] = useState(false);
 
@@ -13,56 +22,103 @@ const MainBar = () => {
         setSidebar(!sidebar)
         setBlack(!black)
     }
+
+    const LOGOUT = () => {
+        dispatch(userActions.logOut());
+        history.replace('/login');
+        window.location.reload();
+        alert("로그아웃 되었습니다.")
+    }
     
     return (
-        <MainBarCSS>
-            <div className="header">
-                <Link to ="/">
-                    <img src="images/fielddbylogo.png" alt="home" className="header-logo"/>
-                </Link>
-                <div className="header-wrapper">
-                    <button>
-                        <img src="images/search.png" alt="search" className="header-search"/>
-                    </button>
-                    <button>
-                        <img src="images/hamburger.png" alt="menu" className="header-menu" onClick={showSidebar}/>
-                    </button>
-                </div>
-            </div>
-            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-                <ul className='nav-menu-items' onClick={showSidebar}>
-                    <li className='navbar-toggle'>
-                        <Link to="#" className='menu-bars'>
-                            <img src="images/close.png" alt="close" className="btn-close" onClick={showSidebar}/>
+        <>
+            {is_login 
+                ? (
+                <MainBarCSS>
+                    <div className="header">
+                        <Link to ="/">
+                            <img src="images/fielddbylogo.png" alt="home" className="header-logo"/>
                         </Link>
-                    </li>
-                    {SidebarData.map((item, index) => {
-                        return (
-                            <li key={index} className={item.cName}>
-                                <Link to={item.path}>
-                                    <span>{item.title}</span>
+                        <div className="header-wrapper">
+                            <button>
+                                <img src="images/search.png" alt="search" className="header-search"/>
+                            </button>
+                            <button>
+                                <img src="images/hamburger.png" alt="menu" className="header-menu" onClick={showSidebar}/>
+                            </button>
+                        </div>
+                    </div>
+                    <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                        <ul className='nav-menu-items' onClick={showSidebar}>
+                            <li className='navbar-toggle'>
+                                <Link to="#" className='menu-bars'>
+                                    <img src="images/close.png" alt="close" className="btn-close" onClick={showSidebar}/>
                                 </Link>
                             </li>
-                        )
-                    })}
-                </ul>
-                <Link to="/login">
-                    <div className="login-container">
-                        <div className="login-area" onClick={showSidebar}>로그인</div>    
-                    </div>           
-                </Link>
-            </nav>
-            <div className={black ? 'black active' : 'black'} onClick={showSidebar}></div>
-        </MainBarCSS>
+                            {LoginSidebarData.map((item, index) => {
+                                return (
+                                    <li key={index} className={item.cName}>
+                                        <Link to={item.path}>
+                                            <span>{item.title}</span>
+                                        </Link>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                        <div className="logout">
+                            <button className="logout-btn" onClick={LOGOUT}>
+                                로그아웃
+                            </button>
+                        </div>
+                    </nav>
+                    <div className={black ? 'black active' : 'black'} onClick={showSidebar}></div>
+                </MainBarCSS>
+            ) : (
+                <MainBarCSS>
+                    <div className="header">
+                        <Link to ="/">
+                            <img src="images/fielddbylogo.png" alt="home" className="header-logo"/>
+                        </Link>
+                        <div className="header-wrapper">
+                            <button>
+                                <img src="images/search.png" alt="search" className="header-search"/>
+                            </button>
+                            <button>
+                                <img src="images/hamburger.png" alt="menu" className="header-menu" onClick={showSidebar}/>
+                            </button>
+                        </div>
+                    </div>
+                    <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                        <ul className='nav-menu-items' onClick={showSidebar}>
+                            <li className='navbar-toggle'>
+                                <Link to="#" className='menu-bars'>
+                                    <img src="images/close.png" alt="close" className="btn-close" onClick={showSidebar}/>
+                                </Link>
+                            </li>
+                            {IntroSidebarData.map((item, index) => {
+                                return (
+                                    <li key={index} className={item.cName}>
+                                        <Link to={item.path}>
+                                            <span>{item.title}</span>
+                                        </Link>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </nav>
+                    <div className={black ? 'black active' : 'black'} onClick={showSidebar}></div>
+                </MainBarCSS>
+            )}
+        </>
     );
 }
 
 const MainBarCSS = styled.div`
     .header{
-        height: 60px;
+        height: 50px;
         position: fixed;
         top: 0;
-        width: calc(100% - 24px);
+        width: calc(100% - 18px);
         max-width: 600px;
         background-color: #fff;
         display: flex;
@@ -73,16 +129,17 @@ const MainBarCSS = styled.div`
         padding-right: 18px;
         z-index : 1000;
         .header-logo{
-            height: 48px;
+            height: 40px;
         }
         .header-wrapper{
             display: flex;
             justify-content: space-between;
+            align-items : flex-start;
             .header-search{
-                height: 22px;
+                height: 21px;
             }
             .header-menu{
-                height: 20px;
+                height: 19px;
             }
             button {
                 border : none;
--- src/Components/Common/Mainbar/MainbarData.jsx
+++ src/Components/Common/Mainbar/MainbarData.jsx
@@ -1,24 +1,56 @@
-import React from "react";
-
-export const SidebarData = [
+export const LoginSidebarData = [
     {
         title : 'STYLE',
         path : '/style',
         cName : 'nav-text'
     },
     {
-        title : 'STORE',
+        title : 'SHOP',
         path : '/store',
         cName : 'nav-text'
     },
     {
-        title : '찜',
-        path : '/bookmark',
+        title : '공지사항',
+        path : '/notice',
+        cName : 'nav-text'
+    },
+    {
+        title : '자주 묻는 질문',
+        path  : '/quesiton',
         cName : 'nav-text'
     },
     {
         title : '마이페이지',
         path : '/mypage',
         cName : 'nav-text'
+    }
+]
+
+
+export const IntroSidebarData = [
+    {
+        title : 'STYLE',
+        path : '/style',
+        cName : 'nav-text'
     },
+    {
+        title : 'SHOP',
+        path : '/store',
+        cName : 'nav-text'
+    },
+    {
+        title : '공지사항',
+        path : '/notice',
+        cName : 'nav-text'
+    },
+    {
+        title : '자주 묻는 질문',
+        path  : '/quesiton',
+        cName : 'nav-text'
+    },
+    {
+        title : '로그인',
+        path : '/login',
+        cName : 'nav-text'
+    }
 ]
