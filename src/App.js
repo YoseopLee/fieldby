e2b841e66962011393a58584055d4ff7746ea9eb
YoseopLee <80019249+YoseopLee@users.noreply.github.com>
2022-01-10 16:12:24 +0900

    [add]mypage-ing

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import {
   Route,
   BrowserRouter,
 } from "react-router-dom";
+import PrivateRoute from "./Components/Common/PrivateRoute/PrivateRoute";
 import OAuth2RedirectHandler from "./Components/Login/KaKao/OAuth2RedirectHandler";
 import OAuth2RedirectHandlerNaver from "./Components/Login/Naver/OAuth2RedirectHandlerNaver";
 
 import Home from "./Pages/Home/Home";
 import Item from "./Pages/Item/Item";
 import Login from "./Pages/Login/Login";
+import Mypage from "./Pages/Mypage/Mypage";
 import PostEach from "./Pages/Post/PostEach";
 import PostNew from "./Pages/Post/PostNew";
 import PostPop from "./Pages/Post/PostPop";
@@ -20,14 +22,19 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
+
         <Route path="/style" element={<PostPop/>}/>
         <Route path="/style-new" element={<PostNew />} />
         <Route path="/post/:id" element={<PostEach/>} />
+
         <Route path="/store" element={<Store/>} />
         <Route path="/item/:id" element={<Item/>} />
+
         <Route path="/login" element={<Login />} />
         <Route path="/Api/Member/Oauth2ClientCallback/kakao/" element={<OAuth2RedirectHandler />}/>
         <Route path="/Api/Member/Oauth2ClientCallback/naver/" element={<OAuth2RedirectHandlerNaver />} />
+
+        <Route path="/mypage" element={<Mypage/>} />
       </Routes>
     </BrowserRouter>
   );
